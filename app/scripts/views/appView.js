f08ebef0d83cd45a1af5348e081d5d34542824c7
Shannon Wynn <shannonwynn@mac.com>
2014-10-31 10:26:11 -0400

    Initial Commit of Backbone Scaffold

---
+++ app/scripts/views/appView.js
@@ -0,0 +1,30 @@
+/*global BlogBackbone, Backbone, JST*/
+
+BlogBackbone.Views = BlogBackbone.Views || {};
+
+(function () {
+    'use strict';
+
+    BlogBackbone.Views.Appview = Backbone.View.extend({
+
+        template: JST['app/scripts/templates/appView.ejs'],
+
+        tagName: 'div',
+
+        id: '',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+        }
+
+    });
+
+})();
